acd938df627484ac6f577cd33c1d6c1d359dd24e
Aaron <Badbatman4@yahoo.com>
2025-12-01 07:14:25 -0600

    created authentication route and sign-in form component alongside sign-up form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -21,6 +21,10 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
+    const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+    };
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -30,18 +34,25 @@ const SignUpForm = () => {
         }
 
         try {
-            const { user } = await createAuthUserWithEmailAndPassword(email, password);
+            const { user } = await createAuthUserWithEmailAndPassword(
+                email, 
+                password
+            );
 
             await createUserDocumentFromAuth(user, { displayName });
-            setFormFields(defaultFormFields);
-            
+            resetFormFields();
         } catch (error) {
-            console.log("User creation encountered an error", error);
+            if (error.code === 'auth/email-already-in-use') {
+                alert('Cannot create user, email already in use');
+            } else {
+                console.log("User creation encountered an error", error);
+            }
         }
     };
 
     const handleChange = (event) => {
         const { name, value } = event.target;
+        
         setFormFields({ ...formFields, [name]: value });
     };
 
