800a6e545d90fd9a87deac662a0a45047d3fb625
Aaron <Badbatman4@yahoo.com>
2025-12-01 05:56:15 -0600

    created sign up form and button components, refactored directory component location

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,14 +3,10 @@ import {
     getAuth, 
     signInWithRedirect, 
     signInWithPopup, 
-    GoogleAuthProvider 
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from "firebase/auth";
-import { 
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc 
-} from "firebase/firestore";
+import { getFirestore, doc, getDoc,setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB8CdFcgMnNR5qzRrDJBNf9eakgg4GheMk",
@@ -31,10 +27,15 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth , 
+  additionalInformation = {displayName: 'Aaron'}
+) => {
+  if (!userAuth) return;
   const userDocRef = doc(db, 'users', userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
@@ -48,10 +49,16 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInformation
       });
     } catch (error) {
       console.log('error creating the user', error.message);
     }
   }
   return userDocRef;
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
 }
