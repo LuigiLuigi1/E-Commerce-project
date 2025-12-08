ff2e5c0ef8557cecf129697780d533091be200b5
Aaron <Badbatman4@yahoo.com>
2025-12-08 10:30:27 -0600

    changed styles to JSX files and restructured components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import { 
     signInWithGooglePopup, 
@@ -88,7 +88,9 @@ const SignInForm = () => {
                 />
                 <div className="buttons-container">
                     <Button type="submit">Sign In</Button>
-                    <Button buttonType="google" onClick={signInWithGoogle}>
+                    <Button buttonType={BUTTON_TYPE_CLASSES.google}
+                    type="button"
+                    onClick={signInWithGoogle}>
                         Google sign in
                     </Button>
                 </div>
