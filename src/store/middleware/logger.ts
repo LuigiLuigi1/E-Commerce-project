ead5e1b576b9738226e2e6752a069e9b65647a6a
Aaron <Badbatman4@yahoo.com>
2025-12-20 11:56:56 -0600

    Convert JavaScript files to TypeScript files and add type annotations where necessary.

---
+++ src/store/middleware/logger.ts
@@ -0,0 +1,16 @@
+import { Middleware } from 'redux';
+import { RootState } from '../root-reducer';
+
+export const loggerMiddleware: Middleware<{}, RootState> = (store) => (next) => (action: any) => {
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log('type: ', action.type);
+    console.log('payload:', action.payload);
+    console.log('current state:', store.getState());
+
+    next(action);
+
+    console.log('next state:', store.getState());
+};
